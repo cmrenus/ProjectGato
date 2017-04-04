06f12805391b7cd8ffa686a7fdf01c939bda701f
cmrenus <cmrenus@gmail.com>
2017-04-04 12:50:16 -0400

    active tab color is set to the starting default palette. Doesnt change with settings change yet

--- src/client/header/mainHeaderCtrl.js
+++ src/client/header/mainHeaderCtrl.js
@@ -5,13 +5,18 @@
 
 class mainHeaderCtrl{
 
-	constructor($mdSidenav, $log, $location){
+	constructor($mdSidenav, $log, $location, $mdTheming, $mdColors){
 		'ngInject';
 
 		this._$mdSidenav = $mdSidenav;
 		this._$log = $log;
 		this.toggleRight = this.buildToggler('right');
     this.active = $location.path().split('/')[0];
+    console.log($mdTheming.THEMES[$mdTheming.defaultTheme()].colors.accent);
+    var name = $mdTheming.THEMES[$mdTheming.defaultTheme()].colors.accent.name;
+    var hue = $mdTheming.THEMES[$mdTheming.defaultTheme()].colors.accent.hues.default;
+    console.log($mdColors.getThemeColor(name + '-' + hue + '-.8'));
+    this.activeBackgroundColor = $mdColors.getThemeColor(name + '-' + hue + '-.8');
 	};
     /**
      * Build handler to open/close a SideNav; when animation finishes
@@ -37,7 +42,7 @@ class mainHeaderCtrl{
       };
     }
 }
-mainHeaderCtrl.$inject = ['$mdSidenav', '$log', '$location'];
+mainHeaderCtrl.$inject = ['$mdSidenav', '$log', '$location', '$mdTheming', '$mdColors'];
 
 export default mainHeaderCtrl;
 //})();
