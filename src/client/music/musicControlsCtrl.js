7dcae42667596688f72db178fe06b6cce7673435
explodingunicorn <coreyrobinson@Coreys-MacBook-Pro.local>
2017-04-11 14:32:48 -0400

    Searching for local music

--- src/client/music/musicControlsCtrl.js
+++ src/client/music/musicControlsCtrl.js
@@ -1,11 +1,48 @@
+var jsmediatags = require('jsmediatags');
+var fs = require('fs');
+var jetpack = require('fs-jetpack');
+var { dialog, app } = require('electron').remote;
+
+var Song = function(data, path) {
+    this.artist = data.artist || 'Unknown Artist';
+    this.album = data.album || 'Unknown Album';
+    this.title = data.title || 'Unknown Title';
+    this.track = parseInt(data.track) || 0;
+
+    var image = data.picture;
+    if(image) {
+        var base64String = "";
+        for (var i = 0; i < image.data.length; i++) {
+            base64String += String.fromCharCode(image.data[i]);
+        }
+        var base64 = "data:" + image.format + ";base64," +
+                window.btoa(base64String);
+        this.picture = base64;
+    }
+    else {
+        this.picture = './img/albumPlaceHolder.png';
+    }
+    // this.number = data.track.no;
+    // this.albumImg = data.picture[0].data;
+    // this.albumImgExtension = data.picture[0].format;
+
+    this.path = path;
+}
 
 export default class musicControlsCtrl {
 	constructor(colorService, $scope){
+		this.slash = (function() {
+					if(process.platform === 'darwin' || process.platform === 'linux') {
+							return '/';
+					}
+					else return '\\'
+		})();
 		'ngInject';
 		this.primaryColor = colorService.getThemeColor('primary', 'default');
 		this.accentColor = colorService.getThemeColor('accent', 'default');
 		this.status = 'paused';
 		this.volume = 35;
+		this.library = [];
 		var vm = this;
 		$scope.$watch(function(){return colorService.getThemeColor('primary', 'default')}, function(newVal, oldVal, scope){
 	      if(newVal){
@@ -26,6 +63,69 @@ export default class musicControlsCtrl {
 
 	pause(){
 		this.status = 'paused';
+	}
+
+	getMusicData(path) {
+			//Setting a variable for the controller
+			var musicController = this;
+			//Hands back a promise to be resolved
+			return new Promise(function(resolve, reject) {
+					//using jsmediatags to read our music file data
+					jsmediatags.read(path, {
+							onSuccess: function(data) {
+									musicController.library.push(new Song(data.tags, path));
+									//Resolve the promise
+									resolve();
+							},
+							onError: function(error) {
+									reject();
+							}
+					})
+			});
+	}
+	//Function to search our directory, takes a path, an empty array for promises, and a callback function
+	searchDir(path, promises, callback) {
+			//Getting an array of our files
+			var fileList = jetpack.list(path);
+			for(var i = 0; i < fileList.length; i++) {
+					//Getting the path of the file in list
+					var file = path + this.slash + fileList[i];
+
+					//If the file we are given is actually a folder
+					if(fs.lstatSync(file).isDirectory()) {
+							//Recurse and run search directory again
+							this.searchDir(file, promises);
+					}
+					//If the file is an mp3
+					else if(file.substr(file.length - 4) === '.mp3') {
+							//Push returned data to our promise array
+							promises.push(this.getMusicData(file));
+					}
+			}
+
+			//If there is a callback
+			if(callback) {
+					//Run our callback function
+					callback(promises);
+			}
+	}
+
+	openFileExplorer() {
+		var musicController = this;
+		dialog.showOpenDialog({properties: ['openDirectory']}, function(files) {
+				//Getting the path to our files
+				console.log('hi');
+				var musicFolder = files[0];
+				//Creating an empty array for promises
+				var promises = []
+				//Run our search directory function, give it a callback
+				musicController.searchDir(musicFolder, promises, function() {
+						//This checks all of the promises that were added to our promise array
+						Promise.all(promises).then(function() {
+								console.log(musicController.library);
+						});
+				});
+		});
 	}
 };
 
